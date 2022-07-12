9c22e835e7f94d2bd3682f826edf41894af78e81
Em-Arce <emjenarce@hotmail.com>
2022-07-12 20:22:01 +0800

    render DOM element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello Em</h1>;
+const page = (
+      <nav>
+        <h1>React Challenge</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+);
 const root = document.getElementById('root');
+console.log(page);
 
-ReactDOM.render(element, root);
+ReactDOM.render(page, root);
