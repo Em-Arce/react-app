18c38e8f2b3c892cbbc485f3d528d42e34659d69
Em-Arce <emjenarce@hotmail.com>
2022-07-13 19:41:40 +0800

    Render image from public folder

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './assets/images/logo192.png';
+// import logo from './assets/images/logo192.png';
 
 // create assets folder inside src
 //under src/assets create images and styles folder
 //add all styling in styles/global-css file. then call the className in respective html element
 // https://create-react-app.dev/docs/adding-images-fonts-and-files
 //  https://stackoverflow.com/questions/37644265/correct-path-for-img-on-react-js
+// render image from public folder: https://stackoverflow.com/questions/47196800/reactjs-and-images-in-public-folder
 
 const page = (
       <div>
-        <img src={logo} width='40px'></img>
+        <img src='/logo512.png' width='40px'></img>
         <h1>Fun facts about React</h1>
         <ul>
             <li>First released in 2013</li>
