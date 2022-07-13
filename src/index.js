4b708b3222fb2d142b1ad687abae140fa62e395a
Em-Arce <emjenarce@hotmail.com>
2022-07-13 19:29:57 +0800

    Render image from src/assets/images folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './assets/images/logo192.png';
+
+// create assets folder inside src
+//under src/assets create images and styles folder
+//add all styling in styles/global-css file. then call the className in respective html element
+// https://create-react-app.dev/docs/adding-images-fonts-and-files
+//  https://stackoverflow.com/questions/37644265/correct-path-for-img-on-react-js
 
 const page = (
-      <nav>
-        <h1>React Challenge</h1>
+      <div>
+        <img src={logo} width='40px'></img>
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>First released in 2013</li>
+            <li>Originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-      </nav>
+      </div>
 );
 const root = document.getElementById('root');
 console.log(page);
