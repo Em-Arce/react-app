846320f38ea98ffb250701a1c5f37cac8edd7d97
Em-Arce <emjenarce@hotmail.com>
2022-07-15 19:29:50 +0800

    Project 1

--- src/Components/RIAMain.js
+++ src/Components/RIAMain.js
@@ -2,16 +2,16 @@ import style from "../assets/styles/global_css.css";
 
 function Main(){
     return(
-      <div className="main--container">
-        <h1>Fun facts about React</h1>
-        <ul>
+      <main className="main--container">
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--facts">
           <li>Was first released in 2013</li>
           <li>Was originally created by Jonathan Walke</li>
-          <li>has well over 100K stars on Github</li>
+          <li>Has well over 100K stars on Github</li>
           <li>Is maintained by Facebook</li>
           <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-      </div>
+      </main>
     )
   };
 
